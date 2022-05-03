30e774179294d4deb8bad1f788a123957dbb34cc
Andrew Burgess <andrew@burgess.io>
2022-05-02 20:51:35 -0400

    wip

--- src/list-parser.ts
+++ src/list-parser.ts
@@ -1,37 +1,35 @@
-
+type List = Array<string | List>;
 
 const last = <T>(arr: T[]): T => arr[arr.length - 1];
 
-
-type List = Array<string | List>;
-
 export default function parse(list: string): any[] {
-  const ret: List = [];
-
-  for (const char of list.slice(1, -1)) {
-    if (char === '(') {
-      ret.push([]);
-    } else if (char === ')') {
-    } else if (char === ' ') {
-    } else {
-      let el = last(ret);
-      if (typeof el === 'string') {
-        ret[ret.length - 1] = el + char;
-      } else {
-        while (Array.isArray(last(el))) {
-
-        }
-
-      }
-
-
-
-      while (Array.isArray(el)) el = last(el);
-
-      el
-
-    }
-  }
+  const chars = list.trim().split('');
+  const [_, ret] = parseList(chars, []);
 
   return ret;
+}
+
+function parseList(chars: string[], list: List): [string[], List] {
+  while(chars.length > 0) {
+    let char = chars.pop();
+
+    if (!char) throw new Error("char not a char");
+
+    if (char === '(') {
+      const [newChars, newList] = parseList(chars, []);
+      chars = newChars;
+      list.push(newList);
+    } else if (char === ')') {
+      return [chars, list];
+    } else if (char === ' ') {
+      list.push('');
+    } else {
+      if (list.length > 0) {
+        list[list.length - 1] = list[list.length  - 1] + char;
+      } else {
+        list.push(char);
+      }
+    }
+  }
+  return  [chars, list];
 }
